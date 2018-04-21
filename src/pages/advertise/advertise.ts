a4c047de5a82d7aa4dcedb3fc072a5d225f8c385
sm910423 <markovic.stanko12@gmail.com>
2018-04-21 05:05:17 +0200

    advertise with us page

--- src/pages/advertise/advertise.ts
+++ src/pages/advertise/advertise.ts
@@ -1,20 +1,28 @@
 import { Component } from '@angular/core';
 import { NavController, NavParams } from 'ionic-angular';
+import { Validators, FormGroup, FormControl } from '@angular/forms';
 
 @Component({
   selector: 'page-advertise',
   templateUrl: 'advertise.html',
 })
 export class AdvertisePage {
+  advertise: FormGroup;
 
   constructor (
     public navCtrl: NavController, 
     public navParams: NavParams
   ) {
+    this.advertise = new FormGroup({
+      email: new FormControl('', Validators.required),
+      mobile: new FormControl(''),
+      name: new FormControl(''),
+      company: new FormControl(''),
+    });
   }
 
   goToBack() {
     this.navCtrl.pop();
   }
-  
+
 }
